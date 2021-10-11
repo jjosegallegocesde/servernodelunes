b4a05462fd6e87fc6f9198848c5be163fe553caf
JuanJo <jjosegallegomesa@gmail.com>
2021-10-11 07:47:38 -0500

    se crea controlador

---
+++ controllers/controlador.js
@@ -0,0 +1,57 @@
+//EL CONTROLADOR ES EL ENCARGADO
+//DE ADMINISTRAR LAS PETICIONES Y RESPUESTAS
+
+const { request, response } = require('express')
+
+//crear una funcion para cada operacion del servidor
+
+function registrarJugador(peticion = request, respuesta = response) {
+
+    respuesta.json({
+        mensaje: "estoy registrando un jugador"
+    })
+
+}
+
+function buscarJugador(peticion = request, respuesta = response) {
+
+    respuesta.json({
+        mensaje: "estoybuscando un jugador"
+    })
+
+}
+
+function buscarJugadores(peticion = request, respuesta = response) {
+
+    respuesta.json({
+        mensaje: "estoy buscando todos los jugadores"
+    })
+
+}
+
+function editarJugador(peticion = request, respuesta = response) {
+
+    respuesta.json({
+        mensaje: "estoy editando un jugador"
+    })
+
+}
+
+function eliminarJugador(peticion = request, respuesta = response) {
+
+    respuesta.json({
+        mensaje: "estoy eliminando un jugador"
+    })
+
+}
+
+module.exports = {
+
+    registrarJugador,
+    buscarJugador,
+    buscarJugadores,
+    editarJugador,
+    eliminarJugador
+
+}
+
